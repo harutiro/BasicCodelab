a295cdee1db1178bb4945f7f2691df4743af8c0e
harutiro <harutiro2027@outlook.jp>
2022-08-06 20:23:12 +0900

    update:最終処理完了

--- app/src/main/java/net/harutiro/basiccodelab/MainActivity.kt
+++ app/src/main/java/net/harutiro/basiccodelab/MainActivity.kt
@@ -1,19 +1,26 @@
 package net.harutiro.basiccodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.harutiro.basiccodelab.ui.theme.BasicCodelabTheme
@@ -37,11 +44,14 @@ fun MyApp() {
     //画面回転をしてもその状態を保持してくれる。
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
-    if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-    } else {
-        Greetings()
+    Surface(){
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
     }
+
 }
 
 @Composable
@@ -74,51 +84,120 @@ private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
     }
 }
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-
-//    //ぬるっと動くアニメーション
-//    val extraPadding by animateDpAsState(
-//        if (expanded.value) 48.dp else 0.dp
-//    )
-
-    //バネのように動くアニメーション
-    val extraPadding by animateDpAsState(
-        if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    //kwskURL
-    //https://developer.android.com/jetpack/compose/animation?hl=ja
-
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
+private fun Greeting(name: String) {
+    Card(
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name)
+        CardContent(name)
+    }
+}
+
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.displaySmall.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
-            Button(
-                onClick = { expanded.value = !expanded.value },
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
         }
     }
 }
 
 
+//@Composable
+//fun Greeting(name: String) {
+//    val expanded = remember { mutableStateOf(false) }
+//
+////    //ぬるっと動くアニメーション
+////    val extraPadding by animateDpAsState(
+////        if (expanded.value) 48.dp else 0.dp
+////    )
+//
+//    //バネのように動くアニメーション
+//    val extraPadding by animateDpAsState(
+//        if (expanded.value) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+//    )
+//
+//    //kwskURL
+//    //https://developer.android.com/jetpack/compose/animation?hl=ja
+//
+//    Surface(
+//        color = MaterialTheme.colorScheme.surface,
+//        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+//    ) {
+//        Row(modifier = Modifier.padding(24.dp)) {
+//            Column(
+//                modifier = Modifier
+//                    .weight(1f)
+//                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+//            ) {
+//                Text(text = "Hello, ")
+//                Text(
+//                    text = name,
+//                    //文字の大きさと太さを設定
+//                    style = MaterialTheme.typography.displaySmall.copy(
+//                        fontWeight = FontWeight.ExtraBold
+//                    )
+//
+//                )
+//            }
+//            Button(
+//                onClick = { expanded.value = !expanded.value },
+//            ) {
+//                Text(if (expanded.value) "Show less" else "Show more")
+//            }
+//        }
+//    }
+//}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
@@ -127,29 +206,27 @@ fun DefaultPreview() {
     }
 }
 
-//@Composable
-//fun OnboardingScreen() {
-//    // TODO: This state should be hoisted
-//    var shouldShowOnboarding by remember { mutableStateOf(true) }
-//
-//    Surface {
-//        Column(
-//            modifier = Modifier.fillMaxSize(),
-//            verticalArrangement = Arrangement.Center,
-//            horizontalAlignment = Alignment.CenterHorizontally
-//        ) {
-//            Text("Welcome to the Basics Codelab!")
-//            Button(
-//                modifier = Modifier.padding(vertical = 24.dp),
-//                onClick = { shouldShowOnboarding = false }
-//            ) {
-//                Text("Continue")
-//            }
-//        }
-//    }
-//}
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320,)
+@Composable
+fun GreetingPreview() {
+    BasicCodelabTheme {
+        Greetings()
+    }
+}
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320,)
 @Composable
 fun OnboardingPreview() {
     BasicCodelabTheme {
